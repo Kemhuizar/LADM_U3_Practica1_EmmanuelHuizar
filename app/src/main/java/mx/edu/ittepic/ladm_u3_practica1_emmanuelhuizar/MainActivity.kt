c4365fb8c24b43e0c81ed1d892a17078fb5d0bd6
Kemhuizar <keemhuizarag@ittepic.edu.mx>
2020-04-10 22:08:36 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica1_emmanuelhuizar/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.ladm_u3_practica1_emmanuelhuizar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
